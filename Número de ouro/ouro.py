22f466edbeee8b3a5a47135209fb58309e7b0bbc
Ariel Tadeu da Silva <silva.ariel2013@gmail.com>
2020-08-11 17:22:14 -0300

    Retângulo e espiral com a sequência de Fibonacci

--- Número de ouro/ouro.py
+++ Número de ouro/ouro.py
@@ -41,89 +41,6 @@ class Abertura(Scene):
 ############################################
 # Cena intermediárias
 ############################################
-
-# GRID from theoremofbeethoven
-class Grid(VGroup):
-    CONFIG = {
-        "height": 7.0,
-        "width": 7.0,
-    }
-
-    def __init__(self, rows, columns, **kwargs):
-        digest_config(self, kwargs, locals())
-        super().__init__(**kwargs)
-
-        x_step = self.width / self.columns
-        y_step = self.height / self.rows
-
-        for x in np.arange(0, self.width + x_step, x_step):
-            self.add(Line(
-                [x - self.width / 2., -self.height / 2., 0],
-                [x - self.width / 2., self.height / 2., 0],
-            ))
-        for y in np.arange(0, self.height + y_step, y_step):
-            self.add(Line(
-                [-self.width / 2., y - self.height / 2., 0],
-                [self.width / 2., y - self.height / 2., 0]
-            ))
-
-
-class ScreenGrid(VGroup):
-    CONFIG = {
-        "rows": 8,
-        "columns": 14,
-        "height": FRAME_Y_RADIUS * 2,
-        "width": 14,
-        "grid_stroke": 0.5,
-        "grid_color": WHITE,
-        "axis_color": RED,
-        "axis_stroke": 2,
-        "labels_scale": 0.25,
-        "labels_buff": 0,
-        "number_decimals": 2
-    }
-
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        rows = self.rows
-        columns = self.columns
-        grid = Grid(width=self.width, height=self.height, rows=rows, columns=columns)
-        grid.set_stroke(self.grid_color, self.grid_stroke)
-
-        vector_ii = ORIGIN + np.array((- self.width / 2, - self.height / 2, 0))
-        vector_si = ORIGIN + np.array((- self.width / 2, self.height / 2, 0))
-        vector_sd = ORIGIN + np.array((self.width / 2, self.height / 2, 0))
-
-        axes_x = Line(LEFT * self.width / 2, RIGHT * self.width / 2)
-        axes_y = Line(DOWN * self.height / 2, UP * self.height / 2)
-
-        axes = VGroup(axes_x, axes_y).set_stroke(self.axis_color, self.axis_stroke)
-
-        divisions_x = self.width / columns
-        divisions_y = self.height / rows
-
-        directions_buff_x = [UP, DOWN]
-        directions_buff_y = [RIGHT, LEFT]
-        dd_buff = [directions_buff_x, directions_buff_y]
-        vectors_init_x = [vector_ii, vector_si]
-        vectors_init_y = [vector_si, vector_sd]
-        vectors_init = [vectors_init_x, vectors_init_y]
-        divisions = [divisions_x, divisions_y]
-        orientations = [RIGHT, DOWN]
-        labels = VGroup()
-        set_changes = zip([columns, rows], divisions, orientations, [0, 1], vectors_init, dd_buff)
-        for c_and_r, division, orientation, coord, vi_c, d_buff in set_changes:
-            for i in range(1, c_and_r):
-                for v_i, directions_buff in zip(vi_c, d_buff):
-                    ubication = v_i + orientation * division * i
-                    coord_point = round(ubication[coord], self.number_decimals)
-                    label = Text(f"{coord_point}",font="Arial",stroke_width=0).scale(self.labels_scale)
-                    label.next_to(ubication, directions_buff, buff=self.labels_buff)
-                    labels.add(label)
-
-        self.add(grid, axes, labels)
-
-
 class Algebra(Scene):      
     def construct(self):
         # Exibindo título e movendo para o topo
@@ -286,7 +203,69 @@ class Algebra(Scene):
         )
         self.wait(1)
 
-class Geometria(Scene):      
+class Geometria(Scene):   
+    def desenhar_fibonacci(self, n, wait_time = 0, multiplicador = 1, color = "#43bfca") -> VGroup:
+        try:
+            if n < 1:
+                raise ValueError
+
+            # Usado para calcular os numeros de Fibonacci    
+            fibonacci = lambda x: 1 if x <= 1 else fibonacci(x-1) + fibonacci(x-2)
+
+            quadrados = VGroup()
+            lados = {
+                0:DOWN,
+                1:RIGHT,
+                2:UP,
+                3:LEFT,
+            }
+
+            # Cria o primeiro quadrado
+            tamanho = multiplicador
+            quadrados.add(Square(side_length = tamanho))
+            quadrados[-1].next_to(ORIGIN, DR, buff=0)
+            
+            # Se necessário, cria o restante
+            if n > 1:
+                for i in range(1,n):
+                    tamanho = fibonacci(i) * multiplicador
+                    quadrados.add(Square(side_length = tamanho))
+                    quadrados[-1].next_to(quadrados[:i], lados[i%4], buff=0)
+
+            # Deixa a figura centralizada na tela e exibe animação
+            quadrados.move_to(ORIGIN)
+            quadrados.set_color(color)
+            for i in range(n):
+                self.play(
+                    ShowCreation(quadrados[i])
+                )
+                self.wait(wait_time)
+            return quadrados
+
+        except ValueError:
+            print("Valor inserido é inválido! Tente com um valor maior ou igual a 1")
+
+    def desenhar_arcos(self, quadrados, color = "#F2E33A") -> VGroup:        
+        arcos = VGroup()
+        i = 0
+        for quadrado in quadrados:
+            pontos = quadrado.get_vertices()
+            # Necessário para fazer a animação do arco no sentido correto
+            if i % 2:
+                arco = ArcBetweenPoints(pontos[(i+2) % 4], pontos[i % 4])
+            else:
+                arco = ArcBetweenPoints(pontos[i % 4], pontos[(i+2) % 4])
+            arcos.add(arco)
+            i += 1
+
+        arcos.set_color(color)
+        self.play(
+            ShowCreation(arcos, run_time = 2)
+        )
+        return arcos
+
+            
+
     def construct(self):
         # Exibindo título e movendo para o topo
         titulo = TexMobject("\\text{Número de ouro}")
@@ -298,9 +277,6 @@ class Geometria(Scene):
         titulo_top.scale(1)
         titulo_top.to_edge(UP)
 
-        grid=ScreenGrid()
-        self.add(grid)
-
         # Animações
         self.play(
             FadeIn(titulo),
@@ -311,6 +287,27 @@ class Geometria(Scene):
             ReplacementTransform(titulo, titulo_top),
         )
         self.wait(1.5)
+
+        # Desenhando os quadrados e os arcos
+        quadrados = self.desenhar_fibonacci(8, multiplicador=.25)
+        self.desenhar_arcos(quadrados)
+        self.wait(3)
+
+        # Exibindo tamanho
+        x = Brace(quadrados, DOWN)
+        y = Brace(quadrados, LEFT)
+        self.play(
+            FadeIn(y),
+            FadeIn(x),
+        )
+        
+
+        # Exibe o valor
+        # formula - TexMobject(
+        #     '\\phi = ',
+        # )
+
+        self.wait(5) 
 
 
 
